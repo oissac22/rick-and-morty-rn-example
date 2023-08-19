c7746860aa1cef2f5e1bc6f5466c0ae17947683f
unknown <oissac22@hotmail.com>
2023-08-19 19:30:16 -0300

    upgrad design buttons

--- src/components/CButton.tsx
+++ src/components/CButton.tsx
@@ -1,9 +1,42 @@
-import { Button, ButtonProps } from "react-native";
+import { Button, ButtonProps, StyleSheet, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { COLOR } from "../design";
+import { CText } from "./CText";
 
-interface ICButtonProps extends ButtonProps {}
+const DEFAULT_SIZE = 40;
 
-export function CButton({ color, ...props }: ICButtonProps)
-{
-    return <Button color={color || COLOR} {...props} />
+interface ICButtonProps extends TouchableOpacityProps {
+    title?:string
 }
+
+export function CButton({ children, title, style, ...props }: ICButtonProps)
+{
+    const styleResult = {
+        ...styleCButton.container,
+        ...(style as any || {})
+    }
+
+    return <TouchableOpacity {...props} style={styleResult}>
+        {
+            !title ?
+            children :
+            <CText style={styleCButton.text}>
+                {title}
+            </CText>
+        }
+    </TouchableOpacity>
+}
+
+const styleCButton = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderColor: COLOR,
+        borderRadius: 4,
+        minWidth: DEFAULT_SIZE,
+        minHeight: DEFAULT_SIZE,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        fontSize: 16
+    }
+})
