37470a1c90673c19ef1dbe6490736dddb81a8731
unknown <oissac22@hotmail.com>
2023-08-19 22:42:09 -0300

    errors messages ok

--- src/pages/DetailPC/components/Detalhes.tsx
+++ src/pages/DetailPC/components/Detalhes.tsx
@@ -7,10 +7,26 @@ import { Local } from "./Local";
 import { Episodes } from "./Episodes";
 import { ShowMoreInfo } from "./ShowMoreInfo";
 import { LinkBack } from "../../../components/LinkBack";
+import { useEffect } from "react";
+import { useErrorsMessages } from "../../../components/Errors";
 
+const TIME_ERROR = 15 * 1000;
 
-export function Detalhes() {
-    const { loadding } = useProviderDetailPC();
+export function Detalhes()
+{
+    const { loadding, error:errorText } = useProviderDetailPC();
+    const { error } = useErrorsMessages();
+
+    useEffect(() => {
+        if (errorText)
+        {
+            error({
+                title: "Erro de servidor",
+                text: "Houve algum problema ao tentar se comunicar com o servidor, verifique sua internet.",
+                time: TIME_ERROR 
+            })
+        }
+    },[error, errorText])
 
     if (loadding)
         return null;
